2bd02a92a531a80c5bb93e5c27d25a5ecc876ec2
Gaurav Kumar <ga00av.kumar@gmail.com>
2022-08-09 18:20:05 +0530

    save

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,6 @@ import { ThemeProvider } from '@emotion/react'
 import { createTheme } from '@mui/material'
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import About from '../components/About'
 import Header from '../components/Header'
 import Hero from '../components/Hero'
 
@@ -18,11 +17,10 @@ const Home: NextPage = () => {
     <div style={{fontFamily:'"Roboto","Helvetica","Arial",sans-serif'}}>
       <ThemeProvider theme={theme}>
         <Head>
-          <title>Ccourses | Home</title>
+          <title>CCourses | Learn in demand skill and build your career</title>
         </Head>
         <Header/>
         <Hero />
-        <About/>
       </ThemeProvider>
     </div>
   )
